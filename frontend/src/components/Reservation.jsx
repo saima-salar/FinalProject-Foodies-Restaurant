1acd460984d7f74cc40113b78dc8a2d8a304e142
saima-salar <saimasalar198@gmail.com>
2025-04-15 14:47:15 +0500

    edit

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/reservation/send",
+        "http://localhost:5000/api/v1/reservation/send",
         { firstName, lastName,date, time ,email, phone},
         {
           headers: {
